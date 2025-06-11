c3e39f7a8757a94c5ffca40426f0318fcf49acd8
qinhan <3091533438@qq.com>
2025-06-11 15:21:19 +0800

    feat(upload): 集成阿里云 OSS 实现文件上传功能- 新增 AliyunOSSOperator 类用于操作阿里云 OSS - 添加 AliyunOSSProperties 配置类 - 更新 application.yml配置文件，添加阿里云 OSS 相关配置 - 新增 Demo 类作为阿里云 OSS 使用示例 - 修改 UploadController，支持使用阿里云 OSS 进行文件上传 - 在 pom.xml 中添加阿里云 OSS SDK 依赖

--- tlias-web-management/src/main/java/com/qinhan/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/qinhan/controller/UploadController.java
@@ -1,20 +1,22 @@
 package com.qinhan.controller;
 
 import com.qinhan.pojo.Result;
+import com.qinhan.utils.AliyunOSSOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
 
 @Slf4j
 @RestController
 public class UploadController {
 
+    /**
+     * 本地磁盘存储方案
+     */
+    /*
     @PostMapping("/upload")
     public Result upload(String name, Integer age, MultipartFile file) throws IOException {
         log.info("接收到的参数{},{},{}", name, age, file);
@@ -28,5 +30,18 @@ public class UploadController {
         // 保存文件
         file.transferTo(new File("D:/images/" + newFileName));
         return Result.success();
+    }*/
+
+
+    @Autowired
+    private AliyunOSSOperator aliyunOSSOperator;
+
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("文件上传:{}",file.getOriginalFilename());
+        // 将文件交给OSS来存储管理
+        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
+        log.info("文件上传到OSS，url:{}",url);
+        return Result.success(url);
     }
 }
