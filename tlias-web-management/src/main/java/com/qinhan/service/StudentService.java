087cf819580b9bfec126518c806374f2dc9cd246
qinhan <3091533438@qq.com>
2025-06-16 15:41:20 +0800

    feat(tlias-web-management): 添加学生管理相关代码,搭建学生管理框架
    - 新增 Student 类用于表示学生信息
    - 创建 StudentController 用于处理学生相关请求
    - 添加 StudentMapper 接口用于学生数据的持久化
    - 定义 StudentService 接口和实现类用于学生业务逻辑

---
+++ tlias-web-management/src/main/java/com/qinhan/service/StudentService.java
@@ -0,0 +1,4 @@
+package com.qinhan.service;
+
+public interface StudentService {
+}
---
+++ tlias-web-management/src/main/java/com/qinhan/service/impl/StudentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.qinhan.service.impl;
+
+import com.qinhan.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
